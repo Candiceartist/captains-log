f83552e40d0bd8a092a95012028c2f30c34e18a3
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-07 11:46:43 -0400

    added view enhine and index for html

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.render('index')
 })
 
 app.listen(3000)
