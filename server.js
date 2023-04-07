200d1b356d623f79f8f306e8f3fdb7efb2cee6e0
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-07 12:17:09 -0400

    created routes folder and article js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const articleRouter = require('./routes/articles')
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { articles: articles})
 })
 
 app.listen(3000)
