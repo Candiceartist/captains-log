817b3d4673575dedc74b85afe957e07416dd31ba
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-07 15:18:20 -0400

    mongodb cannected

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const articleRouter = require('./routes/articles')
+
+const mongoURI = process.env.MONGO_URI;
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("open", () => console.log("mongo connected: ", mongoURI));
+db.on("close", () => console.log("mongo disconnected"));
+mongoose.set("strictQuery", true);
+mongoose.connect('mongodb+srv://candiceartist:Livewire286@cluster0.pxtgk1p.mongodb.net/?retryWrites=true&w=majority')
+
+
 
 app.set('view engine', 'ejs')
 
